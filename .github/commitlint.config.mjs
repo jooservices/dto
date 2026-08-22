b0068a7f5becfd67e9f869eb9023ed0560312ca6
Viet Vu <jooservices@gmail.com>
2026-08-22 07:52:36 -0600

    ci: Add GitHub Actions and Dependabot

---
+++ .github/commitlint.config.mjs
@@ -0,0 +1,33 @@
+/** @type {import('@commitlint/types').UserConfig} */
+export default {
+    rules: {
+        'header-max-length': [2, 'always', 120],
+        'header-min-length': [2, 'always', 10],
+        'header-full-stop': [2, 'never', '.'],
+        'type-enum': [
+            2,
+            'always',
+            [
+                'feat',
+                'fix',
+                'docs',
+                'style',
+                'refactor',
+                'perf',
+                'test',
+                'build',
+                'ci',
+                'chore',
+                'revert',
+            ],
+        ],
+        'type-empty': [2, 'never'],
+        'subject-empty': [2, 'never'],
+        'type-case': [2, 'always', 'lower-case'],
+        'header-pattern': [
+            2,
+            'always',
+            /^(feat|fix|docs|style|refactor|perf|test|build|ci|chore|revert)(\([a-z0-9\-./]+\))?(!)?: [A-Z].+$/,
+        ],
+    },
+};
