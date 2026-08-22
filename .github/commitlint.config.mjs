4789f7eefc8573ead28c7428178e5c0caac777e5
Viet Vu <jooservices@gmail.com>
2026-08-22 14:37:07 -0600

    fix: Update Commitlint rules for v6

--- .github/commitlint.config.mjs
+++ .github/commitlint.config.mjs
@@ -1,5 +1,6 @@
 /** @type {import('@commitlint/types').UserConfig} */
 export default {
+    defaultIgnores: true,
     rules: {
         'header-max-length': [2, 'always', 120],
         'header-min-length': [2, 'always', 10],
@@ -24,10 +25,6 @@ export default {
         'type-empty': [2, 'never'],
         'subject-empty': [2, 'never'],
         'type-case': [2, 'always', 'lower-case'],
-        'header-pattern': [
-            2,
-            'always',
-            /^(feat|fix|docs|style|refactor|perf|test|build|ci|chore|revert)(\([a-z0-9\-./]+\))?(!)?: [A-Z].+$/,
-        ],
+        'subject-case': [2, 'always', ['sentence-case']],
     },
 };
